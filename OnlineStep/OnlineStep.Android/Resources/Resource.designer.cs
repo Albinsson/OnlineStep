df3ac84f07c2a05db81d3d63291d9397034c42a9
maosar <oerra90@gmail.com>
2020-03-03 11:05:45 +0100

    Cleanup in ChapterViewModel

--- OnlineStep/OnlineStep.Android/Resources/Resource.designer.cs
+++ OnlineStep/OnlineStep.Android/Resources/Resource.designer.cs
@@ -5812,10 +5812,13 @@ namespace OnlineStep.Droid
 			public const int notify_panel_notification_icon_bg = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int tooltip_frame_dark = 2131165308;
+			public const int splash_logo = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int tooltip_frame_light = 2131165309;
+			public const int tooltip_frame_dark = 2131165309;
+			
+			// aapt resource value: 0x7F07007E
+			public const int tooltip_frame_light = 2131165310;
 			
 			static Drawable()
 			{
--- OnlineStep/OnlineStep/ViewModels/ChapterViewModel.cs
+++ OnlineStep/OnlineStep/ViewModels/ChapterViewModel.cs
@@ -24,7 +24,11 @@ namespace OnlineStep.ViewModels
         private List<ChapterLevels> chapterLevelsList;
 
         private List<ChapterLevels> chapterLevels;
-
+        public ChapterViewModel(INavigator navigator)
+        {
+            Init();
+            _navigator = navigator;
+        }
         public List<ChapterLevels> ChapterLevels
         {
             get => chapterLevels;
@@ -37,101 +41,6 @@ namespace OnlineStep.ViewModels
             chapterLevels = dbHelper.GetChaptersByLevel(Data.Obj.ToString());
         }
 
-        public List<Chapter> ChapterList
-        {
-            get => chapterList;
-            set => chapterList = value;
-        }
-
-        public List<ChapterLevels> ChapterLevelsList
-        {
-            get => chapterLevelsList;
-            set => chapterLevelsList = value;
-        }
-
-        public ChapterViewModel(INavigator navigator)
-        {
-
-            //InitAsyncApiRequest();
-
-            Init();
-
-            _navigator = navigator;
-        }
-
-    
-
-        public void InitAsyncApiRequest()
-        {
-            Data = DataCenter.GetSingletonProcedure("GetChapterID");
-            ChapterList = dbHelper.GetChapters(Data.Obj.ToString());
-            var objList = ChapterList.ConvertAll(x => (object)x);
-            DataCenter.CreateListProcedure("SetChapters", objList);
-            Debug.WriteLine(ChapterList.Count);
-            ChapterLevelDistributor();
-        }
-
-        void ChapterLevelDistributor()
-        {
-            ChapterLevelsList = new List<ChapterLevels>();
-            int index = 1;
-            foreach (var c in ChapterList.Where(c => c.Level.Equals(index.ToString())))
-            {
-                Chapter chap = new Chapter { Author = c.Author, Level = c.Level, Name = c.Name, Pages = c.Pages, Subject = c.Subject, Subjects = c.Subjects, _id = c._id, __v = c.__v };
-                List<Chapter> cList = new List<Chapter> { chap };
-                ChapterLevels cListLevel = new ChapterLevels { ChapterList = cList, Level = index.ToString() };
-                chapterLevelsList.Add(cListLevel);
-                index++;               
-            }
-            ChapterLevelsList.ForEach(i => Console.WriteLine(i.ChapterList.Count));
-            Debug.WriteLine("ChapterLevelList count: " + ChapterLevelsList.Count);
-            Debug.WriteLine(ChapterLevelsList[0].ChapterList[0].Name);
-        }
-
-
-        //void ChapterLevelDistributor()
-        //{
-        //    ChapterLevelsList = new List<ChapterLevels>();
-        //    foreach (var chapter in ChapterList)
-        //    {
-        //        if (ChapterLevelsList.Count == 0)
-        //        {
-        //            List<Chapter> chapList = new List<Chapter>();
-        //            chapList.Add(chapter);
-        //            ChapterLevels chapLvl = new ChapterLevels { ChapterList = chapList, Level = chapter.Level };
-
-        //            ChapterLevelsList.Add(chapLvl);
-        //            Debug.WriteLine("ChapterLevelsList == null" + ChapterLevelsList[0]);
-        //        }
-
-        //            foreach (var level in ChapterLevelsList)
-        //            {
-        //                Debug.WriteLine("For each level");
-        //                if (chapter.Level.Equals(level.Level))
-        //                {
-        //                    Debug.WriteLine("For each level: IF");
-        //                    level.ChapterList.Add(chapter);
-        //                }
-        //                else
-        //                {
-
-        //                    List<Chapter> chapList = new List<Chapter>();
-        //                    chapList.Add(chapter);
-        //                    ChapterLevels chapLvl = new ChapterLevels { ChapterList = chapList, Level = chapter.Level };
-
-        //                    ChapterLevelsList.Add(chapLvl);
-        //                    Debug.WriteLine("For each level: ELSE");
-        //                }
-        //            }
-
-        //    }
-        //    //foreach (var item in ChapterLevelsList)
-        //    //{
-        //    //    Debug.WriteLine("THE ONE AND ONLY DEBUG" + item.Level);
-        //    //}
-        //    Debug.WriteLine("ChapterLevelDistributor END");
-
-        //}
         public ICommand GoToPageView => new Command((id) =>
         {
             List<Models.Page.RootObject> pageList = new List<Models.Page.RootObject>();
